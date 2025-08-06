e8a9ab3ae9a8f689915a81e15203e3a9ed663fd9
Hakan Vardar <hakovardo123@gmail.com>
2025-08-06 13:29:14 +0300

    comments and cleanup at InstanceManager.cpp

--- src/core/InstanceManager.cpp
+++ src/core/InstanceManager.cpp
@@ -8,12 +8,15 @@
 #include <vulkan/vulkan_funcs.hpp>
 #include <vulkan/vulkan_handles.hpp>
 #include <vulkan/vulkan_structs.hpp>
+#include <vulkan/vulkan_to_string.hpp>
 
 #include <cstdint>
 #include <expected>
 #include <vector>
-#include <vulkan/vulkan_to_string.hpp>
 
+/*
+    Checks validation layer support
+*/
 auto checkValidationLayerSupport() -> std::expected<bool, InstanceManagerError> {
     uint32_t layerCount;
     if (auto result = vk::enumerateInstanceLayerProperties(&layerCount, nullptr); result != vk::Result::eSuccess) {
@@ -48,6 +51,10 @@ auto checkValidationLayerSupport() -> std::expected<bool, InstanceManagerError> 
     return true;
 }
 
+/*
+    Gets required extension for our program.
+    If debug mod is enabled adds debug utis extension.
+*/
 auto getRequiredExtensions() -> std::vector<const char*> {
     uint32_t glfwExtensionCount = 0;
     const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
@@ -61,6 +68,49 @@ auto getRequiredExtensions() -> std::vector<const char*> {
     return extensions;
 }
 
+/*
+    Gets all supported extension and logs them if debug mod is enabled.
+*/
+auto getExtensions() -> std::expected<std::vector<vk::ExtensionProperties>, InstanceManagerError> {
+    uint32_t extensionCount = 0;
+    if (auto result = vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        result != vk::Result::eSuccess) {
+        return std::unexpected(InstanceManagerError{.mErrorCode = InstanceManagerError::ErrorCode::eEnumerateExtensions,
+                                                    .mErrorMessage = vk::to_string(result)});
+    }
+
+    std::vector<vk::ExtensionProperties> extensions(extensionCount);
+    if (auto result = vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        result != vk::Result::eSuccess) {
+        return std::unexpected(InstanceManagerError{.mErrorCode = InstanceManagerError::ErrorCode::eEnumerateExtensions,
+                                                    .mErrorMessage = vk::to_string(result)});
+    }
+
+    spdlog::debug("Available Extensions:");
+    for (const auto& extension : extensions) {
+        spdlog::debug("\t{}", extension.extensionName.data());
+    }
+
+    return extensions;
+}
+
+/*
+    Prints debug information about the application info.
+*/
+auto debugApp(vk::ApplicationInfo& appInfo) -> void {
+    spdlog::debug("App Name: {}, Version: {}.{}.{}", appInfo.pApplicationName,
+                  VK_VERSION_MAJOR(appInfo.applicationVersion), VK_VERSION_MINOR(appInfo.applicationVersion),
+                  VK_VERSION_PATCH(appInfo.applicationVersion));
+    spdlog::debug("Engine: {}, Version: {}.{}.{}", appInfo.pEngineName, VK_VERSION_MAJOR(appInfo.engineVersion),
+                  VK_VERSION_MINOR(appInfo.engineVersion), VK_VERSION_PATCH(appInfo.engineVersion));
+    spdlog::debug("Target Vulkan API Version: {}.{}.{}", VK_VERSION_MAJOR(appInfo.apiVersion),
+                  VK_VERSION_MINOR(appInfo.apiVersion), VK_VERSION_PATCH(appInfo.apiVersion));
+}
+
+/*
+    Main function for creating vulkan instance.
+    Sets all required paramters for a vulkan instance.
+*/
 auto InstanceManager::CreateInstance() -> EXPECT_VOID(InstanceManagerError) {
     spdlog::debug("Creating Vulkan instance...");
 
@@ -72,13 +122,7 @@ auto InstanceManager::CreateInstance() -> EXPECT_VOID(InstanceManagerError) {
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    spdlog::debug("App Name: {}, Version: {}.{}.{}", appInfo.pApplicationName,
-                  VK_VERSION_MAJOR(appInfo.applicationVersion), VK_VERSION_MINOR(appInfo.applicationVersion),
-                  VK_VERSION_PATCH(appInfo.applicationVersion));
-    spdlog::debug("Engine: {}, Version: {}.{}.{}", appInfo.pEngineName, VK_VERSION_MAJOR(appInfo.engineVersion),
-                  VK_VERSION_MINOR(appInfo.engineVersion), VK_VERSION_PATCH(appInfo.engineVersion));
-    spdlog::debug("Target Vulkan API Version: {}.{}.{}", VK_VERSION_MAJOR(appInfo.apiVersion),
-                  VK_VERSION_MINOR(appInfo.apiVersion), VK_VERSION_PATCH(appInfo.apiVersion));
+    debugApp(appInfo);
 
     vk::InstanceCreateInfo createInfo{};
     createInfo.sType = vk::StructureType::eInstanceCreateInfo;
@@ -107,16 +151,9 @@ auto InstanceManager::CreateInstance() -> EXPECT_VOID(InstanceManagerError) {
                                                     .mErrorMessage = vk::to_string(result)});
     }
 
-    std::vector<vk::ExtensionProperties> extensions(extensionCount);
-    if (auto result = vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-        result != vk::Result::eSuccess) {
-        return std::unexpected(InstanceManagerError{.mErrorCode = InstanceManagerError::ErrorCode::eEnumerateExtensions,
-                                                    .mErrorMessage = vk::to_string(result)});
-    }
-
-    spdlog::debug("Available Extensions:");
-    for (const auto& extension : extensions) {
-        spdlog::debug("\t{}", extension.extensionName.data());
+    auto extensions = getExtensions();
+    if (!extensions) {
+        return std::unexpected(extensions.error());
     }
 
     auto requiredExtensions = getRequiredExtensions();
@@ -124,7 +161,7 @@ auto InstanceManager::CreateInstance() -> EXPECT_VOID(InstanceManagerError) {
     for (const char* required : requiredExtensions) {
         spdlog::debug("\t{}", required);
         bool found = std::ranges::any_of(
-            extensions, [&](const auto& ext) { return std::string_view(ext.extensionName.data()) == required; });
+            *extensions, [&](const auto& ext) { return std::string_view(ext.extensionName.data()) == required; });
 
         if (!found) {
             return std::unexpected(
